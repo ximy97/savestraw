fa50d1892b917812d2d2f41d14b557fd1b89b84d
MotionPulseWs <kadirkevin@hotmail.com>
2024-04-18 17:50:57 -0500

    media query de nav, footer y home

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
 //menu scrolled
-window.addEventListener('scroll', function() {
-    const navSection = document.querySelector('.navSection');
-  
-    // Check if the scroll position is greater than a specific threshold (adjust as needed)
-    if (window.scrollY > 50) {
-      navSection.classList.add('menuScrolled');  // Add the class if scrolled past threshold
-    } else {
-      navSection.classList.remove('menuScrolled'); // Remove the class if scrolled above threshold
-    }
-  });
-  
+window.addEventListener("scroll", function () {
+  const navSection = document.querySelector(".navSection");
+
+  // Check if the scroll position is greater than a specific threshold (adjust as needed)
+  if (window.scrollY > 50) {
+    navSection.classList.add("menuScrolled"); // Add the class if scrolled past threshold
+  } else {
+    navSection.classList.remove("menuScrolled"); // Remove the class if scrolled above threshold
+  }
+});
+
+
+//menu responsive
+
+const menuToggle = document.querySelector(".menu__toggle");
+const navigation = document.querySelector(".navigation");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("activeMenu");
+  navigation.classList.toggle("activeMenu");
+});
+
 
 
 // Function to add 'show' class to elements when they come into view
 function showOnScroll() {
-    const elements = document.querySelectorAll('.show-on-scroll');
-    elements.forEach(element => {
-        const elementPosition = element.getBoundingClientRect().top;
-        const viewportHeight = window.innerHeight;
-        if (elementPosition < viewportHeight * 0.75) {
-            element.classList.add('show');
-        }
-    });
+  const elements = document.querySelectorAll(".show-on-scroll");
+  elements.forEach((element) => {
+    const elementPosition = element.getBoundingClientRect().top;
+    const viewportHeight = window.innerHeight;
+    if (elementPosition < viewportHeight * 0.75) {
+      element.classList.add("show");
+    }
+  });
 }
 
 // Scroll to adoption form when the button is clicked
-document.querySelector('#adoptButton').addEventListener('click', function() {
-    const adoptionForm = document.querySelector('.adoption-form');
-    adoptionForm.scrollIntoView({ behavior: 'smooth' });
-});
-
+//document.querySelector('#adoptButton').addEventListener('click', function() {
+//    const adoptionForm = document.querySelector('.adoption-form');
+//    adoptionForm.scrollIntoView({ behavior: 'smooth' });
+//});
 
 // Add event listener to call showOnScroll function when page is scrolled
-window.addEventListener('scroll', showOnScroll);
+window.addEventListener("scroll", showOnScroll);
 
 // Call showOnScroll function on page load to check if elements are in view
 showOnScroll();
